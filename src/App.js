6f589df7050eee2af92455687ac9f73717462562
Swiftly <ivecis@live.co.kr>
2020-11-16 19:10:36 +0900

    Add something

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const buttonText = { text: 'Click me' }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,9 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <label class="label" for="name">Enter name:</label>
+        <label className="label" for="name">Enter name:</label>
       <input id="name" type="text" />
-      <button style={{backgroundColor: 'blue', color: 'white'}}>Start</button>
+  <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText.text}</button>
       </header>
     </div>
   );
