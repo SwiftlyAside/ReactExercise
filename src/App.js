8757c4d18d6beec925eab0e1ac8de660f22bd8ca
Swiftly <ivecis@live.co.kr>
2020-11-20 20:19:57 +0900

    Modify CommentDetail.js
    코멘트 component 분리

--- src/App.js
+++ src/App.js
@@ -1,53 +1,14 @@
 import logo from './logo.svg';
-import faker from 'faker';
 import './App.css';
+import CommentDetail from "./CommentDetail";
 
 function App() {
     return (
         <div className="ui container comments">
             <img src={logo} className="App-logo" alt="logo"/>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
+            <CommentDetail />
+            <CommentDetail />
+            <CommentDetail />
         </div>
     );
 }
