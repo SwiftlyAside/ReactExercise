9c128885620a1599a7e616f7b5d6a70c71f3a03c
Swiftly <ivecis@live.co.kr>
2020-11-21 14:57:37 +0900

    Modify CommentDetail.js
    코멘트 component 수정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import faker from 'faker';
 import CommentDetail from "./CommentDetail";
 
 function App() {
     return (
         <div className="ui container comments">
             <img src={logo} className="App-logo" alt="logo"/>
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="Nice blog post!"
+                           avatar={faker.image.avatar()}/>
+            <CommentDetail author="Alex" timeAgo="Today at 6:45PM" content="I like the subject"
+                           avatar={faker.image.avatar()}/>
+            <CommentDetail author="Jane" timeAgo="Yesterday at 4:45PM" content="I like the writing."
+                           avatar={faker.image.avatar()}/>
         </div>
     );
 }
