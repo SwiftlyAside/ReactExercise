78fac3688b205a0ca11d709dc3fbb14e14e94e1c
Swiftly <ivecis@live.co.kr>
2020-11-22 08:08:29 +0900

    Add some elements ApprovalCard 추가

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import faker from 'faker';
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 function App() {
     return (
         <div className="ui container comments">
             <img src={logo} className="App-logo" alt="logo"/>
-            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="Nice blog post!"
-                           avatar={faker.image.avatar()}/>
-            <CommentDetail author="Alex" timeAgo="Today at 6:45PM" content="I like the subject"
-                           avatar={faker.image.avatar()}/>
-            <CommentDetail author="Jane" timeAgo="Yesterday at 4:45PM" content="I like the writing."
-                           avatar={faker.image.avatar()}/>
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="Nice blog post!"
+                               avatar={faker.image.animals()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Alex" timeAgo="Today at 6:45PM" content="I like the subject"
+                               avatar={faker.image.animals()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Jane" timeAgo="Yesterday at 4:45PM" content="I like the writing."
+                               avatar={faker.image.animals()}/>
+            </ApprovalCard>
         </div>
     );
 }
