49ec2871522e2ec3f9622bce09ba12fc28db53cf
Swiftly <ivecis@live.co.kr>
2020-11-28 10:14:26 +0900

    Modify SeasonDisplay.jsx 약간의 수정

--- src/SeasonDisplay.jsx
+++ src/SeasonDisplay.jsx
@@ -4,29 +4,29 @@ class SeasonDisplay extends React.Component {
   constructor(props) {
     super(props);
     this.state = { lat: null, errorMessage: '' };
-
-    window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      positionError => {
-        this.setState({ errorMessage: positionError.message });
-      },
-    );
   }
 
   componentDidMount() {
-    console.log('Rendered');
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      positionError => this.setState({ errorMessage: positionError.message }),
+    );
   }
 
   render() {
+    const getSeason = (lat, month) => {
+      if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+      }
+      return lat > 0 ? 'winter' : 'summer';
+    };
+
+    const season = getSeason(this.state.lat, new Date().getMonth());
+    const text =
+      season === 'winter' ? 'Burr, it is chilly.' : "Let's hit the beach!";
+
     if (!this.state.errorMessage && this.state.lat) {
-      return (
-        <div>
-          Latitude:
-          {this.state.lat}
-        </div>
-      );
+      return <div>{text}</div>;
     }
     if (this.state.errorMessage && !this.state.lat) {
       return (
